0dc3e3aa9a445af1d70d237efbf9860761e943ed
Alexander Shchetinin <71156982+alshfu@users.noreply.github.com>
2020-10-31 18:40:48 +0100

    Update js_bg.js

--- js_bg.js
+++ js_bg.js
@@ -1,11 +1,10 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
 <!-- 
 для заболнения всей странички используйте значение:
 div.main.theme-main
 для заболнения блочной части используйте значение:
 div.section-main
 -->
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
 # для анимации birds
 <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.birds.min.js"></script>
 # для анимации cell
