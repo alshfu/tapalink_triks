4da37e9f9826c07a6f046e9325707d3c8167deec
Alexander Shchetinin <71156982+alshfu@users.noreply.github.com>
2020-10-31 18:34:18 +0100

    Create js_bg.js

---
+++ js_bg.js
@@ -0,0 +1,38 @@
+<!-- 
+для заболнения всей странички используйте значение:
+div.main.theme-main
+для заболнения блочной части используйте значение:
+div.section-main
+-->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+# для анимации birds
+<script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.birds.min.js"></script>
+# для анимации cell
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.cells.min.js"></script>
+# для анимации cluds2
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.clouds2.min.js"></script>
+# для анимации cluds
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.clouds.min.js"></script>
+# для анимации dots
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.dots.min.js"></script>
+# для анимации fog
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.fog.min.js"></script>
+# для анимации globe
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.globe.min.js"></script>
+# для анимации halo
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.halo.min.js"></script>
+# для анимации net
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.net.min.js"></script>
+# для анимации rings
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.rings.min.js"></script>
+# для анимации ripple
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.ripple.min.js"></script>
+# для анимации topology
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.topology.min.js"></script>
+# для анимации truck
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.trunk.min.js"></script>
+# для анимации waves
+<script src="https://cdn.jsdelivr.net/npm/vanta@0.5.21/dist/vanta.waves.min.js"></script>
+
+
